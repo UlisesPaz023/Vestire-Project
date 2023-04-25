66cbb9933a2b4408fe32a1c85ea525b0fd351d32
Ulises <lioipaz2010@gmail.com>
2023-04-25 11:05:45 -0300

    axios && json-server added, card + productgrid updated

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import styles from '../card/card.module.css';
 
-const Card = () => {
+const Card = (props) => {
+  const {id, resumenDescripcion, descripcion, imagen} = props.x;
   return (
-    <div className="card col-2 m-2">
-      <img src="" className="card-img-top" alt="..."/>
+    <div className="card col-2">
+      <img src={imagen} className="card-img-top" alt={resumenDescripcion}/>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">{resumenDescripcion}</h5>
+        <p className="card-text">{descripcion}</p>
+        <a href="#" className="btn btn-primary">COMPRAR AHORA</a>
       </div>
     </div>
   );
--- src/components/productGrid/ProductGrid.jsx
+++ src/components/productGrid/ProductGrid.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from '../card/Card';
+import axios from 'axios';
 
 const ProductGrid = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(()=> {
+    const getData = async () => {
+      const urlBase =  `http://localhost:3000/productos`;
+      try {
+        const {data} = await axios.get(urlBase);
+        setProduct(data);
+      } catch (error) {
+        alert('Ha ocurrido un problema.');
+      }
+    };
+    getData();
+  },[]);
+
   return (
     <div className='container-fluid'>
       <div className='row justify-content-center'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {
+          product.map((x, i)=>(
+            <Card x = {x}/>
+          ))
+        }
       </div>
     </div>
   );
