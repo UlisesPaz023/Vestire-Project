fd12e32589327944e64799778e91f5d68d777a1e
Ulises <lioipaz2010@gmail.com>
2023-04-29 13:12:00 -0300

    se modifica el componente categorías

--- src/components/categories/CategoriesItem.jsx
+++ src/components/categories/CategoriesItem.jsx
@@ -1,42 +1,23 @@
 import React from 'react';
 
 const CategoriesItem = ({products, clase}) => {
-  const arrayCategories = products.reduce((stack, prod) => {
-    return stack.concat(prod.categoria);
-  }, []);
+  // const arrayCategories = products.reduce((stack, prod) => {
+  //   return stack.concat(prod.categoria);
+  // }, []);
 
-  let trueArrayCategories = arrayCategories.filter((x, i) => {
-    return arrayCategories.indexOf(x) === i;
-  });
-
-  const arraySubcategories = products.reduce((stack, prod)=>{
-    return stack.concat(prod.subcategories);
-  })
-
-  
-  // let removeDuplicatesCategories = trueArrayCategories.map((x,i)=> {
-  //   if(products[i].clase.includes(x)){
-  //     return x;
-  //   }
-  // })
-
-  console.log(trueArrayCategories)
+  // let trueArrayCategories = arrayCategories.filter((x, i) => {
+  //   return arrayCategories.indexOf(x) === i;
+  // });
   
   return (
     <div>
       {
-      trueArrayCategories.map((x,i)=> (
-        products.forEach((x,i)=> (
-          products[i].clase.includes(x)
-        ))
+      products.map((x,i)=> (
+        
+        x.clase.includes(clase)
         &&
         x.categoria
       ))
-      // products.map((x,i)=> (
-      //   x.clase.includes(clase)
-      //   &&
-      //   x.categoria
-      // ))
       }
     </div>
   );
