788d0f49ee934ddc97f7b7424391bb556265b9ef
TomasYalaMartinez <Tomasyala18@gmail.com>
2023-05-01 23:49:51 -0300

    cambios en footer y trabado con el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// import React from "react";
 import {
   createBrowserRouter,
   RouterProvider,
   createRoutesFromElements,
-  Route
-} from 'react-router-dom';
-import RootLayout from './layout/RootLayout';
-import { routes } from './routes';
+  Route,
+} from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+import { routes } from "./routes";
+// import NavBar from "./components/navbar/Navbar";
+import Body from "./components/body/Body";
+import Footer from "./components/footer/Footer";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout/>}>
-      {
-        routes.map(({Element, path, index}) => (
-          <Route key={index} path={path} element={<Element/>}/>
-        ))
-      }
+    <Route element={<RootLayout />}>
+      {routes.map(({ Element, path, index }) => (
+        <Route key={index} path={path} element={<Element />} />
+      ))}
     </Route>
   )
 );
@@ -25,9 +26,12 @@ const router = createBrowserRouter(
 function App() {
   return (
     <>
+      <Body />
+      {/* <NavBar /> */}
       <RouterProvider router={router} />
+      <Footer />
     </>
   );
-};
+}
 
 export default App;
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./body.css";
+
+const Body = () => {
+  return (
+    <>
+      <div className="body">
+        <div className="body-tittle">
+          <h1>holiS</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Body;
