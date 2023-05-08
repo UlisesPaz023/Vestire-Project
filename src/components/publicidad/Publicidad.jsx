5437988e3ffdb0763f36ea12c454efc8c9897e20
unknown <lazarteelias10@gmail.com>
2023-05-08 16:13:45 -0300

    modificando la imagende la publicidad

--- src/components/publicidad/Publicidad.jsx
+++ src/components/publicidad/Publicidad.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import style from '../publicidad/publicidad.module.css'
+import publicidad from '../img/publicidad7.jpg'
 
 function Publicidad() {
   return (
   <div className='container-fluid'>
-        <div className='row d-flex'>
-            <img className={`${style.img} p-0`} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTITVTP3hJvCCSqEYHOIczuT-du3dHJQSgk_w&usqp=CAU" alt="publicidad" />
+        <div className='row d-flex img-fluid'>
+            <img className={`${style.img} p-0 my-2`} src={publicidad} alt="publicidad" />
         </div>
   </div>
   )
